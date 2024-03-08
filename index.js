28752ec3d6c6205eda8314bc239f5fbae6e1ee56
samitkumarpatel-work <SamitKumar.Patel@maersk.com>
2024-03-08 16:44:30 +0100

    POST endpoint and message/all endpoint

--- index.js
+++ index.js
@@ -3,10 +3,27 @@ const app = express()
 var cors = require('cors')
 const port = 3000
 
+app.use(express.json()) //json
+app.use(express.raw()) // ?
+app.use(express.text()) //text
+app.use(express.urlencoded({ extended: true })) //form data or url-encoded
 app.use(cors())
+
+const message = [];
 
 app.get('/message', (req, res) => {
   res.json({ message: 'Hello World from api!'})
+})
+
+app.post('/message', (req, res) => {
+  if(req.body) {
+    message.push(req.body)
+  }
+  res.status(201).json({ message: 'Message created!'});
+})
+
+app.get('/message/all', (req, res) => {
+  res.json(message);
 })
 
 
