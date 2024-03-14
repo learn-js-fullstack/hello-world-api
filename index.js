8d5f67cdedd641f67330b7862b7a58630770aa22
samitkumarpatel-work <SamitKumar.Patel@maersk.com>
2024-03-14 16:29:58 +0100

    delete api

--- index.js
+++ index.js
@@ -9,21 +9,37 @@ app.use(express.text()) //text
 app.use(express.urlencoded({ extended: true })) //form data or url-encoded
 app.use(cors())
 
-const message = [];
+const messages = [{"message": '1'},{"message": '2'}];
 
 app.get('/message', (req, res) => {
-  res.json({ message: 'Hello World from api!'})
+  res.json({ msg: 'Hello World from api!'})
 })
 
 app.post('/message', (req, res) => {
   if(req.body) {
-    message.push(req.body)
+    messages.push(req.body)
   }
-  res.status(201).json({ message: 'Message created!'});
+  res.status(201).json({ msg: 'Message created!'});
 })
 
 app.get('/message/all', (req, res) => {
-  res.json(message);
+  res.json(messages);
+})
+
+app.delete('/message/:text', (req, res) => {
+  const textToRemove = req.params.text;
+  
+  const index = messages.findIndex((o) => o.message === textToRemove)
+
+  console.log(`text: ${textToRemove}, index: ${index}`)
+
+  if(index !== -1) {
+    messages.splice(index, 1);
+    res.json({msg: 'Message deleted!'});
+  } else {
+    res.status(404).json({msg: 'Message not found!'});
+  }
+  
 })
 
 
